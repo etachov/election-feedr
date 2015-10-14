d6ceab5f6528623692bb2ee1a9f030ba1a4a31fe
Evan <evan.tachovsky@gmail.com>
2015-10-14 14:32:36 -0400

    Adding script, updating README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # election-feedr
-Pull upcoming elections from IFES' Election Calendar and filter down to countries of interest
+
+This script (1) pulls upcoming elections from IFES' [Election Guide](http://www.electionguide.org/) Calendar, (2) filter the elections against a vector of countries supplied by the user to produce a final list of upcoming elections in countries of interest.
+
+I use this script to keep tabs on upcoming elections in countries where [MDIF](http://www.mdif.org) has investments. When an election takes place, we like to evaluate how our client's coverage compares to other media sources. We visualize the results in our [Election StoryMap](http://www.mdif.org/client-election-coverage/) feature. 
+
+## Next Steps: 
+* Replace the .csv write out with code that updates a Google Sheet using the great [googlesheet](https://github.com/jennybc/googlesheets) package.
+* Refactor the whole nodeGet section. This can be smoother and it's a problem I encounter semi-regularly so worth it to spend some time on.
+
---
+++ election-feedr.R
@@ -0,0 +1,48 @@
+
+library(dplyr) # general data manipulation
+library(XML) # to parse the xml feed
+library(countrycode) # convert country names to country codes. working with country names always gets messy so i like to convert to codes ASAP
+
+## parse the raw xml from IFES's Election Guide RSS feed into the R data type XMLInternalDocument
+feed_raw <- xmlParse("http://www.electionguide.org/feed/calendar/upcoming", encoding = "UTF-8")
+
+## make a list of the nodes we want to pull out
+node_list <- as.list(c("//item/title", "//item/link", "//item/description", "//item/pubDate"))
+
+## helper function that usings xpathSApply to extact the value from each node using xmlValue
+nodeGet <- function(x) {
+  x <- xpathSApply(feed_raw, x, xmlValue)
+  x
+}
+
+## apply the function to the list of nodes and turn the result into a data.frame
+data_raw <- as.data.frame(lapply(node_list, nodeGet)) 
+
+## set the names using the original list
+names(data_raw) <- gsub("//item/", "", unlist(node_list))
+
+## make a vector of countries we want to check the feed data against
+## in practice i read a list of countries in from our internal database, but this works for an example
+country_filter <- c("ARG", "BLR")
+
+## clean up the data with some regex/dplyr
+data <- data_raw %>%
+  mutate(country = gsub(":.*", "", title),
+         iso3c = countrycode(country, "country.name", "iso3c"),
+         elect.type = gsub(".*:", "", title),
+         # remove the html from the description
+         description = gsub("<.*?>|&nbsp;", "", description),
+         # because i always forget the symbols for the format function http://statmethods.net/input/dates.html
+         date = as.Date(pubDate, format = "%a, %d %b %Y")) %>%
+  select(country, iso3c, elect.type, date, description, link) %>%
+  # filter down to only the country we care about
+  filter(iso3c %in% country_filter)
+
+## write the data out to csv file with the date accessed in the title
+write.csv(data, paste("upcoming_elections", paste(Sys.Date(), ".csv", sep = ""), sep = "_"), row.names = F)
+
+
+
+
+
+
